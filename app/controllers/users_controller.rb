7dd19a0a84c352974224f33d9dc7833bdef5b86a
Alouiscious Media <info@reneesoul.com>
2019-11-18 00:14:50 -0800

    planning sample and limit of displayed tourdate items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,25 +6,25 @@ class UsersController < ApplicationController
     @user = User.find(session[:user_id])
     @venues = Venue.visible
     @tourdates = Tourdate.all
-
-
     erb :'/tours/tourdates.html'
   end
 
   get 'tourdates/new' do
     Helpers.redirect_if_not_logged_in(session)  
-    @venues = Venue.all
+    @venues = Venue.visible
     @tourdates =Tourdate.all
     @user = Helpers.current_user(session)
+    binding.pry
 
-    if !params[:user].keys.include?("venue_ids")
-      params[:user]["venue_ids"] = []
-      # @user = User.find_by_id(params[:id])
-    end
-    @user = User.create(params[:user])
-    if !params[:user].empty?
-      @user.venues << Venue.create(name: params[:user])
-    end
+
+    # if !params[:user].keys.include?("venue_ids")
+    #   params[:user]["venue_ids"] = []
+    #   # @user = User.find_by_id(params[:id])
+    # end
+    # @user = User.create(params[:user])
+    # if !params[:user].empty?
+    #   @user.venues << Venue.create(name: params[:user])
+    # end
 
 
     erb :'tours/new.html'
@@ -45,35 +45,35 @@ class UsersController < ApplicationController
     if !params[:user].empty?
       @user.venues << Venue.create(name: params[:user])
     end
-    redirect '/tourdates'
+    redirect '/tourdates/new'
   end
 
-  get '/tourdates/:id' do    # renders the user's collected tour dates
-    Helpers.redirect_if_not_logged_in(session)             
-    # this route posts to :id/edit
-    # includes an edit option for the user's tourdates
-    erb :'/tours/tourdates'    
-  end
+  # get '/tourdates/:id' do    # renders the user's collected tour dates
+  #   Helpers.redirect_if_not_logged_in(session)             
+  #   # this route posts to :id/edit
+  #   # includes an edit option for the user's tourdates
+  #   erb :'/tours/edit.html'    
+  # end
 
-  post '/tourdates/:id/edit' do
-    @user = User.find(session[:user_id])
-    @venues = Venue.all
-    @tourdates = Tourdate.all
+  # post '/tourdates/:id/edit' do
+  #   @user = User.find(session[:user_id])
+  #   @venues = Venue.all
+  #   @tourdates = Tourdate.all
 
-  end
+  # end
 
-  patch '/tourdates/:id' do
-    Helpers.redirect_if_not_logged_in(session)    
-    @venues = Venue.all
-    @user = Helpers.current_user(session)
-    @tourdates = Tourdates.all
+  # patch '/tourdates/:id' do
+  #   Helpers.redirect_if_not_logged_in(session)    
+  #   @venues = Venue.all
+  #   @user = Helpers.current_user(session)
+  #   @tourdates = Tourdates.all
 
-  end
+  # end
 
-  delete '/tourdates/:id' do
-    session.clear
-    redirect '/'
-  end
+  # delete '/tourdates/:id' do
+  #   session.clear
+  #   redirect '/'
+  # end
 
 
   # get tourdates           venues - lists all and links to venues details(venue/:id and venue/show) 
